83d47f5259fc8d3b0ecb7f0577d9c573510391db
Stevie Robinson <stevie.robinson@gmail.com>
2025-08-13 22:10:30 +0200

    reverseString test

---
+++ __tests__/reverseString.test.js
@@ -0,0 +1,21 @@
+﻿import { reverseString } from '../reverseString.js'
+
+test("Reverse Hello", () => {
+  expect(reverseString('Hello')).toBe('olleH');
+})
+
+test("Reverse Goodbye", () => {
+  expect(reverseString('Goodbye')).toBe('eybdooG');
+})
+
+test("Reverse two words", () => {
+  expect(reverseString('Hello Sir')).toBe('riS olleH');
+})
+
+test("Reverse numbers", () => {
+  expect(reverseString('12345')).toBe('54321');
+})
+
+test("Reverse string with multiple spaces", () => {
+  expect(reverseString('hello   hello')).toBe('olleh   olleh');
+})
