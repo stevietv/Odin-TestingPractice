cafd5b223663507dff963fee8ccff069c5f7ccc1
Stevie Robinson <stevie.robinson@gmail.com>
2025-08-14 19:44:28 +0200

    analyzeArray tests

---
+++ __tests__/analyzeArray.test.js
@@ -0,0 +1,23 @@
+﻿import { analyzeArray } from '../analyzeArray.js';
+
+test('single length array, all items should be 1', () => {
+  let expected = { average: 1, min: 1, max: 1, length: 1 };
+  let result = analyzeArray([1]);
+  expect(result).toEqual(expected);
+})
+
+test('array of items 1 to 3', () => {
+  let expected = { average: 2, min: 1, max: 3, length: 3 };
+  let result = analyzeArray([1,2,3]);
+  expect(result).toEqual(expected);
+})
+
+test('array of three 1s and one 5', () => {
+  let expected = { average: 2, min: 1, max: 5, length: 4 };
+  let result = analyzeArray([1,1,1,5]);
+  expect(result).toEqual(expected);
+})
+
+test('array contains a string - should error', () => {
+    expect(() => analyzeArray(['hello'])).toThrow('Expected an array of numbers')
+})
---
+++ analyzeArray.js
@@ -0,0 +1,16 @@
+﻿export function analyzeArray(arr) {
+  if (!arr.every( function(element) { return typeof element === 'number'; })) {
+    throw new Error('Expected an array of numbers');
+  }
+  let length = arr.length;
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let average = +(arr.reduce((a, b) => a + b, 0)/ length).toFixed(2)  //use the + to convert back to number
+
+  return {
+    average: average,
+    min: min,
+    max: max,
+    length: length,
+  }
+}
